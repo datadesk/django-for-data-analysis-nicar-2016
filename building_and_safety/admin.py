6dfeff5f1c08f3a547a29e544f4bd79d64e40c5c
Armand Emamdjomeh <armand.emamdjomeh@latimes.com>
2015-02-17 12:21:13 -0800

    and tweaking admin as well

--- building_and_safety/admin.py
+++ building_and_safety/admin.py
@@ -1,9 +1,8 @@
-from django.contrib.gis import admin
+from django.contrib import admin
 from building_and_safety.models import *
-from django.contrib.gis.admin import OSMGeoAdmin
 
 # Register your models here.
-class ComplaintAdmin(OSMGeoAdmin):
+class ComplaintAdmin(admin.ModelAdmin):
     list_display = ('csr','full_address','date_received','date_closed','days_since_complaint','past_due_date','days_past_due_date','is_closed','more_than_one_year','lat_visited')
     list_filter = ['is_closed', 'more_than_one_year','area_planning_commission','lat_visited','investigate_further', "past_due_date"]
     search_fields = ['csr','full_address']
